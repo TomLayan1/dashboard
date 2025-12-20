c23962ceead9e4a5da5130ff7e11e0edc89d7a5a
TomLayan1 <afolayantomisin1919@gmail.com>
2025-12-20 18:06:06 +0100

    Added the security and privacy section

--- src/app/components/DashboardComponents/Settings/AppAndDevices.tsx
+++ src/app/components/DashboardComponents/Settings/AppAndDevices.tsx
@@ -1,9 +1,55 @@
+import {
+  PhoneIcon,
+  Bell,
+  Shield,
+  PaintRoller,
+  ChevronRight,
+} from "lucide-react"
+
 export default function AppAndDevices() {
   return (
     <div>
       <p className="text-slate-500 mb-7">APP & DEVICES</p>
-      <div className="p-5 bg-[#3e3e3ea1] rounded-3xl">
+      <div className=" flex flex-col gap-5.5 p-5 bg-[#3e3e3ea1] rounded-3xl">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-5">
+            <div className="bg-[#3e3e3ecf] flex items-center justify-center py-1.5 px-1.5 rounded-md">
+              <Shield size={15} />
+            </div>
+            <p className="text-[15px]">Consent Request</p>
+          </div>
+          <ChevronRight size={15} />
+        </div>
 
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-5">
+            <div className="bg-[#3e3e3ecf] flex items-center justify-center py-1.5 px-1.5 rounded-md">
+              <PhoneIcon size={15} />
+            </div>
+            <p className="text-[15px]">Change Password</p>
+          </div>
+          <ChevronRight size={15} />
+        </div>
+
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-5">
+            <div className="bg-[#3e3e3ecf] flex items-center justify-center py-1.5 px-1.5 rounded-md">
+              <Bell size={15} />
+            </div>
+            <p className="text-[15px]">Notifications</p>
+          </div>
+          <ChevronRight size={15} />
+        </div>
+
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-5">
+            <div className="bg-[#3e3e3ecf] flex items-center justify-center py-1.5 px-1.5 rounded-md">
+              <PaintRoller size={15} />
+            </div>
+            <p className="text-[15px]">Theme</p>
+          </div>
+          <ChevronRight size={15} />
+        </div>
       </div>
     </div>
   )
--- src/app/components/DashboardComponents/Settings/SecurityAndPrivacy.tsx
+++ src/app/components/DashboardComponents/Settings/SecurityAndPrivacy.tsx
@@ -1,9 +1,66 @@
+import { 
+  ChevronRight,
+  LockIcon,
+  Fingerprint,
+  EyeOffIcon, 
+} from "lucide-react"
+
 export default function SecurityAndPrivacy() {
   return(
     <div>
       <p className="text-slate-500 mb-7">SECURITY AND PRIVACY</p>
-      <div className="p-5 bg-[#3e3e3ea1] rounded-3xl">
-        
+      <div className="w-full flex flex-col gap-5.5 p-5 bg-[#3e3e3ea1] rounded-3xl">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-5">
+            <div className="bg-[#3e3e3ecf] flex items-center justify-center py-1.5 px-1.5 rounded-md">
+              <LockIcon size={15} />
+            </div>
+            <p className="text-[15px]">Change Password</p>
+          </div>
+          <ChevronRight size={15} />
+        </div>
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-5">
+            <div className="bg-[#3e3e3ecf] flex items-center justify-center py-1.5 px-1.5 rounded-md">
+              <LockIcon size={15} />
+            </div>
+            <div>
+              <p className="text-[15px]">Reset User PIN</p>
+              <p className="text-[13px] text-slate-400">For authorising transaction on mobile and web app</p>
+            </div>
+          </div>
+          <ChevronRight size={15} />
+        </div>
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-5">
+            <div className="bg-[#3e3e3ecf] flex items-center justify-center py-1.5 px-1.5 rounded-md">
+              <ChevronRight size={15} />
+            </div>
+            <p className="text-[15px]">Login Options</p>
+          </div>
+          <ChevronRight size={15} />
+        </div>
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-5">
+            <div className="bg-[#3e3e3ecf] flex items-center justify-center py-1.5 px-1.5 rounded-md">
+              <Fingerprint size={15} />
+            </div>
+            <div>
+              <p className="text-[15px]">Reset User PIN</p>
+              <p className="text-[13px] text-slate-400">For authorising transaction on mobile and web app</p>
+            </div>
+          </div>
+          <ChevronRight size={15} />
+        </div>
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-5">
+            <div className="bg-[#3e3e3ecf] flex items-center justify-center py-1.5 px-1.5 rounded-md">
+              <EyeOffIcon size={15} />
+            </div>
+            <p className="text-[15px]">Hide Balance</p>
+          </div>
+          <ChevronRight size={15} />
+        </div>
       </div>
     </div>
   )
