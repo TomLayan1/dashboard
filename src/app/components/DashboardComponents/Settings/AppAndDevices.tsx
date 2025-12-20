6a6659f650fcc01ee78c06c95b64566bd3f1b97e
TomLayan1 <afolayantomisin1919@gmail.com>
2025-12-20 18:26:20 +0100

    Added the app and devices section in the settings page

--- src/app/components/DashboardComponents/Settings/AppAndDevices.tsx
+++ src/app/components/DashboardComponents/Settings/AppAndDevices.tsx
@@ -10,45 +10,45 @@ export default function AppAndDevices() {
   return (
     <div>
       <p className="text-slate-500 mb-7">APP & DEVICES</p>
-      <div className=" flex flex-col gap-5.5 p-5 bg-[#3e3e3ea1] rounded-3xl">
-        <div className="flex items-center justify-between">
+      <div className=" flex flex-col gap-5. p-3 bg-[#3e3e3ea1] rounded-3xl">
+        <div className="flex items-center justify-between p-3 cursor-pointer rounded-md hover:bg-[#3e3e3ecf] duration-200 ease-in-out">
           <div className="flex items-center gap-5">
             <div className="bg-[#3e3e3ecf] flex items-center justify-center py-1.5 px-1.5 rounded-md">
-              <Shield size={15} />
+              <Shield size={15} color="#b81c1c" />
             </div>
             <p className="text-[15px]">Consent Request</p>
           </div>
-          <ChevronRight size={15} />
+          <ChevronRight size={15} color="#b81c1c" />
         </div>
 
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between p-3 cursor-pointer rounded-md hover:bg-[#3e3e3ecf] duration-200 ease-in-out">
           <div className="flex items-center gap-5">
             <div className="bg-[#3e3e3ecf] flex items-center justify-center py-1.5 px-1.5 rounded-md">
-              <PhoneIcon size={15} />
+              <PhoneIcon size={15} color="#b81c1c" />
             </div>
             <p className="text-[15px]">Change Password</p>
           </div>
-          <ChevronRight size={15} />
+          <ChevronRight size={15} color="#b81c1c" />
         </div>
 
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between p-3 cursor-pointer rounded-md hover:bg-[#3e3e3ecf] duration-200 ease-in-out">
           <div className="flex items-center gap-5">
             <div className="bg-[#3e3e3ecf] flex items-center justify-center py-1.5 px-1.5 rounded-md">
-              <Bell size={15} />
+              <Bell size={15} color="#b81c1c" />
             </div>
             <p className="text-[15px]">Notifications</p>
           </div>
-          <ChevronRight size={15} />
+          <ChevronRight size={15} color="#b81c1c" />
         </div>
 
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between p-3 cursor-pointer rounded-md hover:bg-[#3e3e3ecf] duration-200 ease-in-out">
           <div className="flex items-center gap-5">
             <div className="bg-[#3e3e3ecf] flex items-center justify-center py-1.5 px-1.5 rounded-md">
-              <PaintRoller size={15} />
+              <PaintRoller size={15} color="#b81c1c" />
             </div>
             <p className="text-[15px]">Theme</p>
           </div>
-          <ChevronRight size={15} />
+          <ChevronRight size={15} color="#b81c1c" />
         </div>
       </div>
     </div>
--- src/app/components/DashboardComponents/Settings/SecurityAndPrivacy.tsx
+++ src/app/components/DashboardComponents/Settings/SecurityAndPrivacy.tsx
@@ -9,57 +9,57 @@ export default function SecurityAndPrivacy() {
   return(
     <div>
       <p className="text-slate-500 mb-7">SECURITY AND PRIVACY</p>
-      <div className="w-full flex flex-col gap-5.5 p-5 bg-[#3e3e3ea1] rounded-3xl">
-        <div className="flex items-center justify-between">
+      <div className="w-full flex flex-col px-3 py-3 bg-[#3e3e3ea1] rounded-3xl">
+        <div className="flex items-center justify-between px-3 py-1.5 cursor-pointer rounded-md hover:bg-[#3e3e3ecf] duration-200 ease-in-out">
           <div className="flex items-center gap-5">
-            <div className="bg-[#3e3e3ecf] flex items-center justify-center py-1.5 px-1.5 rounded-md">
-              <LockIcon size={15} />
+            <div className="bg-[#3e3e3ecf] flex items-center justify-center rounded-md">
+              <LockIcon size={15} color="#b81c1c" />
             </div>
             <p className="text-[15px]">Change Password</p>
           </div>
-          <ChevronRight size={15} />
+          <ChevronRight size={15} color="#b81c1c" />
         </div>
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between px-3 py-[12.2px] cursor-pointer rounded-md hover:bg-[#3e3e3ecf] duration-200 ease-in-out">
           <div className="flex items-center gap-5">
             <div className="bg-[#3e3e3ecf] flex items-center justify-center py-1.5 px-1.5 rounded-md">
-              <LockIcon size={15} />
+              <LockIcon size={15} color="#b81c1c" />
             </div>
             <div>
               <p className="text-[15px]">Reset User PIN</p>
               <p className="text-[13px] text-slate-400">For authorising transaction on mobile and web app</p>
             </div>
           </div>
-          <ChevronRight size={15} />
+          <ChevronRight size={15} color="#b81c1c" />
         </div>
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between px-3 py-[12.2px] cursor-pointer rounded-md hover:bg-[#3e3e3ecf] duration-200 ease-in-out">
           <div className="flex items-center gap-5">
             <div className="bg-[#3e3e3ecf] flex items-center justify-center py-1.5 px-1.5 rounded-md">
-              <ChevronRight size={15} />
+              <ChevronRight size={15} color="#b81c1c" />
             </div>
             <p className="text-[15px]">Login Options</p>
           </div>
-          <ChevronRight size={15} />
+          <ChevronRight size={15} color="#b81c1c" />
         </div>
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between px-3 py-[12.2px] cursor-pointer rounded-md hover:bg-[#3e3e3ecf] duration-200 ease-in-out">
           <div className="flex items-center gap-5">
             <div className="bg-[#3e3e3ecf] flex items-center justify-center py-1.5 px-1.5 rounded-md">
-              <Fingerprint size={15} />
+              <Fingerprint size={15} color="#b81c1c" />
             </div>
             <div>
               <p className="text-[15px]">Reset User PIN</p>
               <p className="text-[13px] text-slate-400">For authorising transaction on mobile and web app</p>
             </div>
           </div>
-          <ChevronRight size={15} />
+          <ChevronRight size={15} color="#b81c1c" />
         </div>
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between px-3 py-[12.2px] cursor-pointer rounded-md hover:bg-[#3e3e3ecf] duration-200 ease-in-out">
           <div className="flex items-center gap-5">
             <div className="bg-[#3e3e3ecf] flex items-center justify-center py-1.5 px-1.5 rounded-md">
-              <EyeOffIcon size={15} />
+              <EyeOffIcon size={15} color="#b81c1c" />
             </div>
             <p className="text-[15px]">Hide Balance</p>
           </div>
-          <ChevronRight size={15} />
+          <ChevronRight size={15} color="#b81c1c" />
         </div>
       </div>
     </div>
